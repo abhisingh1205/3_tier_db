6f352535a43e5861146f139f08b2f30e8544739e
Abhishek <abhishek08@gmail.com>
2019-11-17 17:16:15 +0530

    db added

---
+++ schema.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `vehicle` (
+  `no` int(11) DEFAULT NULL,
+  `name` varchar(20) DEFAULT NULL,
+  `company` varchar(20) DEFAULT NULL,
+  `type` varchar(20) DEFAULT NULL,
+  `price` varchar(20) DEFAULT NULL,
+  `desciption` varchar(20) DEFAULT NULL
+)
+
+INSERT INTO `vehicle` VALUES (1,'Yamaha R15','Yamaha','2-Wheeler','150000','Bike'),(2,'Pulsar 220','Bajaj','2-Wheeler','150000','Bike'),(3,'ThunderBird 350','Royal Enfield','2-Wheeler','150000','Bike'),(4,'R15 v3','Yamaha','2-Wheeler','150000','Scooty'),(5,'Activa','Honda','2-Wheeler','150000','Scooty');
